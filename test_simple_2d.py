d1c12a844d15532a04dc9778deaddbdb48e4e205
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2016-01-20 20:13:09 +0000

    TST: add pixel tests against the ImageJ plugin.
    So far, these are simple artifitial 2D images only.

--- test_simple_2d.py
+++ test_simple_2d.py
@@ -3,24 +3,27 @@ from __future__ import division, print_function, absolute_import
 import numpy as np
 from numpy.testing import assert_equal
 
+from skel import prepare_image, compute_thin_image
+
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
-from skel import prepare_image, compute_thin_image
 
-
-def check_skel(fname, viz=True):
+def check_skel(fname, viz=False):
+    # compute the thin image and compare the result to that of ImageJ
     img = np.loadtxt('data/' + fname + '.txt', dtype=np.uint8)
 
     if viz:
         ax = _viz(img, **dict(marker='s', color='b', s=99, alpha=0.2))
 
+    # compute
     img1 = prepare_image(img)
     img1 = compute_thin_image(img1)
 
     # undo padding, convert to 2D
     img1_2d = img1[1:-1, 1:-1, 1:-1]
     img1_2d = img1_2d.squeeze()
+    img1_2d *= 255
 
     if viz:
         ax = _viz(img1_2d, ax, **dict(marker='o', color='r',
@@ -29,9 +32,12 @@ def check_skel(fname, viz=True):
     # compare to FIJI
     img_f = np.loadtxt('data/' + fname + '_fiji.txt', dtype=np.uint8)
 
-    if viz:
+    if not viz:
+        # actually compare images
+        assert_equal(img1_2d, img_f)
+    else:
         ax = _viz(img_f, ax, **dict(marker='o', color='g', s=45, label='fiji'))
-    if viz:
+
         ax.legend()
         ax.grid(True)
 
@@ -45,21 +51,24 @@ def check_skel(fname, viz=True):
         plt.show()
 
 
-def _viz(img, ax=None, **kwds):
+# nose test generator
+def test_simple_2d_images():
+    for fname in ("strip", "loop", "cross", "two-hole"):
+        yield check_skel, fname
 
+
+def _viz(img, ax=None, **kwds):
     if ax is None:
         import matplotlib.pyplot as plt
         fix, ax = plt.subplots()
 
     x, y = np.nonzero(img)
     ax.scatter(y, img.shape[1] - x, **kwds)
-
     return ax
 
 
 if __name__ == "__main__":
-#    check_skel('loop')
-    check_skel('cross')
-#    check_skel('two-hole')
-#    check_skel('cross_crop4')
-#    check_skel('strip')
+    import sys
+    if len(sys.argv) < 2:
+        sys.exit("Expect an image name from the data/ directory.")
+    check_skel(sys.argv[1], True)
