f813f55bf81602e9ac19b37bddf07bd28c3a2b24
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2016-02-01 14:11:55 +0000

    ENH: unroll and inline `index_octants` into is_euler_invariant
    13.7/7.3 performance boost on the lobster dataset, huh

--- bench.py
+++ bench.py
@@ -4,15 +4,15 @@
 from IPython import get_ipython
 ipython = get_ipython()
 
-#from skimage import io
+from skimage import io
 from skel import compute_thin_image
 
-#fname = 'data/bat/bat-cochlea-volume.tif'
-#bat = io.imread(fname)
-#compute_thin_image(bat)
+fname = 'data/bat/bat-cochlea-volume.tif'
+bat = io.imread(fname)
+ipython.run_line_magic("timeit", "compute_thin_image(bat)")
 
-import numpy as np
 
-raw = np.fromfile('../lobster.raw', dtype=np.uint8)
-raw = raw.reshape(301, 324, 56)
-ipython.run_line_magic("timeit", "compute_thin_image(raw)")
+#import numpy as np
+#raw = np.fromfile('../lobster.raw', dtype=np.uint8)
+#raw = raw.reshape(301, 324, 56)
+#ipython.run_line_magic("timeit", "compute_thin_image(raw)")
